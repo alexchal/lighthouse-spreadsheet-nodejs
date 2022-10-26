69022e686fa96aa0a940b9cada53eaf73c0b1119
Alexandros <chalvantzis12@outlook.com>
2022-10-26 09:29:47 +0200

    update dependencies and add types for lighthouse

--- config/index.ts
+++ config/index.ts
@@ -1,23 +1,25 @@
+import { LightHouseOptions } from "lighthouse";
+
 const categories = ["performance", "accessibility", "seo", "best-practices"];
 
 export const urls = [
-    "https://www.npmjs.com/",
-    "https://www.npmjs.com/advisories"
+  "https://www.npmjs.com/",
+  "https://www.npmjs.com/advisories"
 ];
 
-export const lightHouseOptions = [
-    {
-        extends: "lighthouse:default",
-        settings: {
-            onlyCategories: [...categories],
-            emulatedFormFactor: "desktop"
-        }
-    },
-    {
-        extends: "lighthouse:default",
-        settings: {
-            onlyCategories: [...categories],
-            emulatedFormFactor: "mobile"
-        }
+export const lightHouseOptions: LightHouseOptions[] = [
+  {
+    extends: "lighthouse:default",
+    settings: {
+      onlyCategories: categories,
+      emulatedFormFactor: "desktop"
     }
+  },
+  {
+    extends: "lighthouse:default",
+    settings: {
+      onlyCategories: categories,
+      emulatedFormFactor: "mobile"
+    }
+  }
 ];
