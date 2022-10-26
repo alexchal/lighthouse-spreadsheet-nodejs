3261f3c655f13a3e7920f61483abd7e1e83b63c1
Alexandros <chalvantzis12@outlook.com>
2022-10-26 10:13:39 +0200

    remove utils

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,16 @@ import dotnev from "dotenv";
 import lighthouse, { FlagsType, LighthouseResultType } from "lighthouse";
 import {
   GoogleSpreadsheet,
-  ServiceAccountCredentials
+  ServiceAccountCredentials,
 } from "google-spreadsheet";
 import { launch } from "chrome-launcher";
 import { urls, lightHouseOptions } from "../config";
-import { formatDate } from "./utils";
 
 dotnev.config();
 
 const credentials: ServiceAccountCredentials = {
   client_email: process.env.CLIENT_EMAIL as string,
-  private_key: (process.env.PRIVATE_KEY as string).replace(/\\n/gm, "\n")
+  private_key: (process.env.PRIVATE_KEY as string).replace(/\\n/gm, "\n"),
 };
 
 const doc = new GoogleSpreadsheet(process.env.SPREADSHEET_ID as string);
@@ -22,16 +21,15 @@ const pushResultsToSpreadSheet = async (results: LighthouseResultType) => {
   const device = results.lhr.configSettings.emulatedFormFactor;
 
   const spreadSheetData = {
-    Date: formatDate(),
+    Date: new Intl.DateTimeFormat("en-GB").format(new Date()),
     Page: results.lhr.requestedUrl,
     Platform: device[0].toUpperCase() + device.slice(1),
     Performance: results.lhr.categories.performance.score * 100,
     "Speed Index": results.lhr.audits["speed-index"].displayValue,
     "Time to Interactive": results.lhr.audits.interactive.displayValue,
     Accessibility: results.lhr.categories.accessibility.score * 100,
-    "Best Practices":
-      results.lhr.categories["best-practices"].score * 100,
-    SEO: results.lhr.categories.seo.score * 100
+    "Best Practices": results.lhr.categories["best-practices"].score * 100,
+    SEO: results.lhr.categories.seo.score * 100,
   };
 
   try {
@@ -45,16 +43,14 @@ const pushResultsToSpreadSheet = async (results: LighthouseResultType) => {
 
 const launchChromeAndRunLighthouse = async (urls: string[]) => {
   const lighthouseResults = async (url: string): Promise<void> => {
-
     for (let i = 0; i < lightHouseOptions.length; i++) {
-
       const chrome = await launch({
-        chromeFlags: ["--headless"]
+        chromeFlags: ["--headless"],
       });
 
       const flags: FlagsType = {
         logLevel: "info",
-        port: chrome.port
+        port: chrome.port,
       };
 
       const options = lightHouseOptions[i];
--- src/utils/index.ts
+++
@@ -1,8 +0,0 @@
-export const formatDate = () => {
-    const date = new Date();
-    const curr_date = date.getDate();
-    const curr_month = date.getMonth();
-    const curr_year = date.getFullYear();
-
-    return `${curr_date}/${curr_month}/${curr_year}`;
-};
