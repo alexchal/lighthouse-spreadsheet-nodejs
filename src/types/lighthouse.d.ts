47169f0259b7d80fa2703789a4b9a9c2a4e7afdc
Alexandros <chalvantzis12@outlook.com>
2022-10-26 14:04:58 +0200

    fix ts-node to include custom types declarations

--- src/types/lighthouse.d.ts
+++ src/types/lighthouse.d.ts
@@ -1,39 +1,38 @@
 declare module "lighthouse" {
-
   type TScore = {
     score: number;
-  }
+  };
 
   type AuditsType = {
     interactive: {
-      displayValue: string
-    },
+      displayValue: string;
+    };
     "speed-index": {
       displayValue: string;
-    }
-  }
+    };
+  };
 
   type CategoriesType = {
     performance: TScore;
     accessibility: TScore;
     seo: TScore;
     "best-practices": TScore;
-  }
+  };
 
-  type LHRTypes = {
+  type LhrTypes = {
     finalUrl: number;
     requestedUrl: string;
     audits: AuditsType;
     categories: CategoriesType;
     configSettings: {
       emulatedFormFactor: string;
-    }
-  }
+    };
+  };
 
   export type FlagsType = {
-    logLevel: "info",
-    port: number
-  }
+    logLevel: "info";
+    port: number;
+  };
 
   export type LightHouseOptions = {
     extends: string;
@@ -41,13 +40,17 @@ declare module "lighthouse" {
       onlyCategories: string[];
       emulatedFormFactor: string;
     };
-  }
+  };
 
   export type LighthouseResultType = {
-    lhr: LHRTypes
-  }
+    lhr: LhrTypes;
+  };
 
-  function lighthouse(url: string, flags: FlagsType, options: LightHouseOptions): LighthouseResultType;
+  function lighthouse(
+    url: string,
+    flags: FlagsType,
+    options: LightHouseOptions,
+  ): LighthouseResultType;
 
   export default lighthouse;
 }
